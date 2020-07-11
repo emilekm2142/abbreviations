893770c5741e24439f5f27372bc5a18178c329d6
emilekm2142 <emilm7843@gmail.com>
2020-07-11 19:28:53 +0200

    bedzie banglac

--- app.ts
+++ app.ts
@@ -4,7 +4,9 @@ var app = express();
 
 let distDir = __dirname + '/dist/';
 app.use(express.static(distDir));
-
+app.get('*', function(req, res) {
+  res.sendfile(distDir+"index.html");
+});
 var server = app.listen(process.env.PORT || 80);
 let fs = require('fs');
 let chance = require('chance').Chance();
